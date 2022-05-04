caa3544cbacb641a81694a901b981ce74ed11962
Damjan15 <stamenovdamjan20@gmail.com>
2022-05-04 01:28:14 -0700

    Pages setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Explore, Offers, Profile, SignIn, SignUp, ForgotPassword } from "./pages"
+
 function App() {
   return (
-    <div>
-      <h1>House Marketplace 🏠</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Explore />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+      </Routes>
+    </Router>
   );
 }
 
